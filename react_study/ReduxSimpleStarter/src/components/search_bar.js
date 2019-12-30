32430229db13468a4cd75986429309067621cf15
ssppoollsem <ssppoollsem@gmail.com>
2019-12-30 17:31:31 +0900

    udemy 15~20

--- react_study/ReduxSimpleStarter/src/components/search_bar.js
+++ react_study/ReduxSimpleStarter/src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
+// 함수형
+// const SearchBar = () => {
+//     return <input />;
+// }
+
+// 클래스형
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term : ''}
+    }
+    render(){
+        return (
+            <div>
+                <input vlue={this.state.term} onChange={event => this.setState({ term : event.target.value }) } />
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
