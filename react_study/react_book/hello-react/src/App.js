fd7528da01a2861cd2a19ba1877b557a89268b29
서재진 <jjseo@itembay.com>
2020-02-06 17:50:08 +0900

    리액트 공부

--- react_study/react_book/hello-react/src/App.js
+++ react_study/react_book/hello-react/src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const name = '리액트';
+	const style = {
+		// background-color 는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성된다.
+		backgroundColor: 'black',
+		color: 'aqua',
+		fontSize: '48px',
+		fontWeight: 'bold',
+		padding: '16' // 단위를 생략하면 px로 지정된다.
+	};
+	return <div style={style}>{name}</div>;
 }
 
 export default App;
