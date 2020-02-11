1d360bb0ad102f55b4a0e1cdfc453901c7ba69a1
서재진 <jjseo@itembay.com>
2020-02-11 16:53:48 +0900

    Revert "리액트 스터디"
    This reverts commit 2542dec9cfafe4cdd734a87f03d753576e422b69.

--- react_study/react_book/hello-react/src/App.js
+++ react_study/react_book/hello-react/src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponet';
-import Counter from './Counter';
-import Say from './Say';
 
 function App(){
-	// return <MyComponent name='3' favoriteNumber={3}>리액트</MyComponent>
-	//return <Counter />
-	return <Say />
+	return <MyComponent name='3' favoriteNumber={3}>리액트</MyComponent>
 }
 
 
