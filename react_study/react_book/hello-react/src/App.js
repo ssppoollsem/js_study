bbc9466969b5f65d184fdff50f95142d579dbb66
서재진 <jjseo@itembay.com>
2020-02-18 12:16:43 +0900

    react_sutdy-state

--- react_study/react_book/hello-react/src/App.js
+++ react_study/react_book/hello-react/src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import MyComponent from './MyComponet';
+
+import Say from './Say';
 
 function App(){
-	return <MyComponent name='3' favoriteNumber={3}>리액트</MyComponent>
+    // return <MyComponent name='3' favoriteNumber={3}>리액트</MyComponent>
+    //return <Counter />
+    return <Say />
 }
-
 
 export default App;
