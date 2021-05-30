9866581ef1fd1445c3dd7e474345b50fce437d4b
서재진 <ssppoollsem@gmail.com>
2021-05-31 04:29:54 +0900

    인프런 강의 학습

--- vue_study/zeroCho/nodebird/ch1/front/store/users.js
+++ vue_study/zeroCho/nodebird/ch1/front/store/users.js
@@ -1,5 +1,7 @@
 export const state = () => ({
     me: null,
+    followerList: [],
+    followingList: [],
 });
 
 // 동기적작업
@@ -8,18 +10,24 @@ export const mutations = {
         // setMe에는 비동기 요청을 쓰면 안됨
         state.me = payload;
     },
+    changeNickname(state, payload) {
+        state.me.nickname = payload.nickname;
+    },
 };
 
 // 비동기작업
 export const actions = {
-    signUp({commit, state}, payload) {
+    signUp({ commit, state }, payload) {
         // 서버에 회원가입 요청을 보내는 부분
         commit('setMe', payload);
     },
-    logIn({commit}, payload) {
+    logIn({ commit }, payload) {
         commit('setMe', payload);
     },
-    logOut({commit}, payload) {
+    logOut({ commit }, payload) {
         commit('setMe', null);
-    }
-}
+    },
+    changeNickname({ commit }, payload) {
+        commit('changeNickname', payload);
+    },
+};
