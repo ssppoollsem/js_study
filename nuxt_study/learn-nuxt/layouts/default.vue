b3a2754b236d90c0d4608264d53e5e92e56e8c61
ITEMBAY\jjseo <jjseo@itembay.com>
2021-12-15 18:07:19 +0900

    인프런 NUXT

---
+++ nuxt_study/learn-nuxt/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header>
+            <h1>{{ $route.name }}페이지</h1>
+            <NuxtLink to="/">홈페이지</NuxtLink>
+            <NuxtLink to="/main">메인 페이지</NuxtLink>
+            <NuxtLink to="/product">프로덕트 페이지</NuxtLink>
+            <!-- <router-link></router-link> 와 동일한 기능을 한다 -->
+        </header>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt_study/learn-nuxt/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>넉스트 시작하기</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
