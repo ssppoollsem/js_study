7181cadac09609e8ad98a9036369405bd94da544
ssppoollsem <ssppoollsem@gmail.com>
2022-01-23 17:09:38 +0900

    영화 검색 사이트 예제 01~07

---
+++ vue_study/fastcampus/vue3-movie-app/src/components/Logo.vue
@@ -0,0 +1,32 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>OMDbAPI</span>.COM
+  </RouterLink>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.logo {
+    font-family: 'Oswald', sans-serif;
+    font-size: 20px;
+    color: $black;
+    text-decoration: none;
+
+    span {
+        color: $primary;
+    }
+
+    &:hover {
+        color: $black;
+    }
+}
+</style>
