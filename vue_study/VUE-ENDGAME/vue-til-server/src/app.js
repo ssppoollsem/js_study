d3f50f546aa45b8fe44e90c1d13b755bff4c7792
ssppoollsem <ssppoollsem@gmail.com>
2022-06-20 21:50:20 +0900

    프론트 프로젝트 생성

--- vue_study/VUE-ENDGAME/vue-til-server/src/app.js
+++ vue_study/VUE-ENDGAME/vue-til-server/src/app.js
@@ -19,9 +19,10 @@ import { authenticateUser } from './utils/auth.js';
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 mongoose.connect(
-  'mongodb+srv://test:1234@cluster0-ypgh5.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://test:1234@cluster0.lae1h.mongodb.net/?retryWrites=true&w=majority', // 몽고디비 application code
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   },
 );
 mongoose.Promise = global.Promise;
