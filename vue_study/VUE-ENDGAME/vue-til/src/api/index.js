ce3bc4767f2c9c38c49343a9aadab30259bff013
ssppoollsem <ssppoollsem@gmail.com>
2022-06-26 21:47:34 +0900

    프로젝트 환경변수  설정

--- vue_study/VUE-ENDGAME/vue-til/src/api/index.js
+++ vue_study/VUE-ENDGAME/vue-til/src/api/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+// axios instance를 통해 공통 설정들을 담는다.
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+});
+
 function registerUser(userData) {
-  const url = 'http://localhost:3000/signup';
-  return axios.post(url, userData);
+  // const url = `${instance}/signup`;
+  // return axios.post(url, userData);
+  return instance.post('signup', userData);
 }
 
 export { registerUser };
