807b0d6b5a60873208c01ee1bc86042f1930a67d
ssppoollsem <ssppoollsem@gmail.com>
2022-06-30 22:25:21 +0900

    학습 노트 데이터 조회 및 로딩스피너 추가

--- vue_study/VUE-ENDGAME/vue-til/src/views/MainPage.vue
+++ vue_study/VUE-ENDGAME/vue-til/src/views/MainPage.vue
@@ -2,12 +2,47 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem.id"
+          :postItem="postItem"
+        ></PostListItem>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+
+export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      this.isLoading = false;
+      // console.log(data.posts);
+      this.postItems = data.posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
