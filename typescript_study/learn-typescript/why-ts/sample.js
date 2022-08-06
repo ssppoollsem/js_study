c1aed4b245a257e54966038cbf05afbba6c40460
ssppoollsem <ssppoollsem@gmail.com>
2022-08-06 18:02:22 +0900

    타입스크립트 스터디

---
+++ typescript_study/learn-typescript/why-ts/sample.js
@@ -0,0 +1,15 @@
+// 자바스크립트 타입스크립트처럼 사용하기
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+sum("10", 20);
