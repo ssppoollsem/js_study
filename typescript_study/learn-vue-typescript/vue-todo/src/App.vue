73b4516b27d8fef5232ab66a56ff173d2be2ff70
ssppoollsem <ssppoollsem@gmail.com>
2022-09-25 15:09:24 +0900

    타입스크립트 & 뷰 - todo 추가기능 구현

--- typescript_study/learn-vue-typescript/vue-todo/src/App.vue
+++ typescript_study/learn-vue-typescript/vue-todo/src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
     <h1>Vue Todo with Typescript</h1>
+    <TodoInput :item="todoText" @input="updateTodoText" @add="addTodoItem"></TodoInput>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import TodoInput from './components/TodoInput.vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: '',
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = '';
+    },
+  },
+});
 </script>
 
 <style scoped></style>
