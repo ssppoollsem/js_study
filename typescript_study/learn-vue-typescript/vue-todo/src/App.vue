c363897e07408fd08b8fb8feed4afcf3eba5920e
ssppoollsem <ssppoollsem@gmail.com>
2022-10-01 14:53:40 +0900

    타입스크립트 & 뷰 - todo 정렬기능 구현

--- typescript_study/learn-vue-typescript/vue-todo/src/App.vue
+++ typescript_study/learn-vue-typescript/vue-todo/src/App.vue
@@ -24,11 +24,11 @@ import TodoListItem from './components/TodoListItem.vue';
 
 const STORAGE_KEY = 'vue-todo-ts-v1';
 const storage = {
-  save(todoItems: any[]) {
+  save(todoItems: Todo[]) {
     const parsed = JSON.stringify(todoItems);
     localStorage.setItem(STORAGE_KEY, parsed);
   },
-  fetch() {
+  fetch(): Todo[] {
     const todoItems = localStorage.getItem(STORAGE_KEY) || '[]';
     const result = JSON.parse(todoItems);
     return result;
@@ -67,7 +67,11 @@ export default Vue.extend({
       this.todoText = '';
     },
     fetchTodoItems() {
-      this.todoItems = storage.fetch();
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+      });
     },
     removeTodo(index: number) {
       this.todoItems.splice(index, 1);
