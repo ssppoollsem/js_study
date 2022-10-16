870f421004d406ee4b289bcd9ae8eaf08c4fdc5c
ssppoollsem <ssppoollsem@gmail.com>
2022-10-16 21:01:38 +0900

    타입스크립트 & 뷰 - 두번째 프로젝트 vuex state, mutations 타입 정의

--- typescript_study/learn-vue-typescript/vue-news/src/store/index.ts
+++ typescript_study/learn-vue-typescript/vue-news/src/store/index.ts
@@ -1,22 +1,31 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
-import getters from './getters.js';
-import mutations from './mutations.js';
-import actions from './actions.js';
+import Vuex, { StoreOptions } from 'vuex';
+import { RootState, state } from './state';
+import { mutations } from './mutations';
+// import getters from './getters.js';
+// import mutations from './mutations.js';
+// import actions from './actions.js';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== 'production',
-  state: {
-    news: [],
-    ask: [],
-    jobs: [],
-    user: {},
-    item: {},
-    list: [],
-  },
-  getters,
-  mutations,
-  actions,
-});
+const store: StoreOptions<RootState> = {
+  state: state,
+  mutations: mutations,
+};
+
+export default new Vuex.Store(store);
+
+// export default new Vuex.Store({
+//   strict: process.env.NODE_ENV !== 'production',
+//   state: {
+//     news: [],
+//     ask: [],
+//     jobs: [],
+//     user: {},
+//     item: {},
+//     list: [],
+//   },
+//   getters,
+//   mutations,
+//   actions,
+// });
