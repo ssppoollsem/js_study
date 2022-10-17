03eeeed8beba7ed21d32d326b0a91a2fff8636f3
ssppoollsem <ssppoollsem@gmail.com>
2022-10-17 23:03:42 +0900

    타입스크립트 & 뷰 - 두번째 프로젝트 스토어 타입 파일 작성

---
+++ typescript_study/learn-vue-typescript/vue-news/src/store/types.ts
@@ -0,0 +1,10 @@
+// store/types.ts
+import { CommitOptions, Store } from 'vuex';
+import { Mutations } from './mutations';
+import { RootState } from './state';
+
+type MyMutations = {
+  commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(key: K, payload?: P, options?: CommitOptions): ReturnType<Mutations[K]>;
+};
+
+export type MyStore = Omit<Store<RootState>, 'commit'> & MyMutations;
