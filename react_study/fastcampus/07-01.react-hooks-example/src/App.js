2a6490c1ce877bdc3686320ae74a7c5ddedd67b9
ssppoollsem <ssppoollsem@gmail.com>
2022-12-03 17:19:12 +0900

    리액트 - react additional hooks

--- react_study/fastcampus/07-01.react-hooks-example/src/App.js
+++ react_study/fastcampus/07-01.react-hooks-example/src/App.js
@@ -8,6 +8,9 @@ import Example5 from './components/Example5';
 import useWindowWidth from './hooks/useWindowWidth';
 import withHasMounted from './hocs/withHasMounted';
 import useHasMounted from './hooks/useHasMounted';
+import Example6 from './components/Exapmel6';
+import Example7 from './components/Exapmel7';
+import Example8 from './components/Example8';
 
 function App({ hasMounted }) {
     const width = useWindowWidth();
@@ -22,7 +25,10 @@ function App({ hasMounted }) {
                 <Example2 />
                 <Example3 />
                 <Example4 /> 
-                <Example5 /> */}
+                <Example5 />
+                <Example6 /> 
+                <Example7 />*/}
+                <Example8 />
                 {width}
             </header>
         </div>
