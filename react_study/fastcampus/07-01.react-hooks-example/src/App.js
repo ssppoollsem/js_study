d6d0e6c439f0967515255dc72e87b0f535ca110f
ssppoollsem <ssppoollsem@gmail.com>
2022-12-03 16:30:02 +0900

    리액트 - react custom hook

--- react_study/fastcampus/07-01.react-hooks-example/src/App.js
+++ react_study/fastcampus/07-01.react-hooks-example/src/App.js
@@ -5,20 +5,28 @@ import Example2 from './components/Example2';
 import Example3 from './components/Example3';
 import Example4 from './components/Example4';
 import Example5 from './components/Example5';
+import useWindowWidth from './hooks/useWindowWidth';
+import withHasMounted from './hocs/withHasMounted';
+import useHasMounted from './hooks/useHasMounted';
 
-function App() {
+function App({ hasMounted }) {
+    const width = useWindowWidth();
+    const hasMountedFromHooks = useHasMounted();
+
+    console.log(hasMounted, hasMountedFromHooks);
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <Example1 />
+                {/* <Example1 />
                 <Example2 />
                 <Example3 />
-                <Example4 />
-                <Example5 />
+                <Example4 /> 
+                <Example5 /> */}
+                {width}
             </header>
         </div>
     );
 }
 
-export default App;
+export default withHasMounted(App);
