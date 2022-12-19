cd4135ea074ab2c3b6ab7425d18ddaf0e0ad8443
ssppoollsem <ssppoollsem@gmail.com>
2022-12-19 21:33:19 +0900

    리액트 - 서버 사이드 렌더링

--- react_study/fastcampus/10-01.tic-tac-toe/server.js
+++ react_study/fastcampus/10-01.tic-tac-toe/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const path = require('path');
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const fs = require('fs');
+
+console.log(
+  ReactDOMServer.renderToString(React.createElement('div', null, 'hello')),
+);
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/test', (req, res) => {
+  const ssr = ReactDOMServer.renderToString(
+    React.createElement('div', null, 'hello'),
+  );
+  const indexHtml = fs
+    .readFileSync(path.join(__dirname, 'build', 'index.html'))
+    .toString()
+    .replace('<div id="root"></div>', `<div id="root">${ssr}</div>`);
+
+  console.log(indexHtml);
+  res.send(indexHtml);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
