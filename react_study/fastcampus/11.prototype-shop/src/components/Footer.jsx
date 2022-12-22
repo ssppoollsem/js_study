1fae8c8fcdb03620c264eaa5c259698530437965
ssppoollsem <ssppoollsem@gmail.com>
2022-12-22 21:01:19 +0900

    리액트 - react 쇼핑몰 프로젝트(기본 디자인 설정)

---
+++ react_study/fastcampus/11.prototype-shop/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+// components/Footer.jsx
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>© 2021 Mark Lee. All rights reserved.</p>
+        </footer>
+    );
+}
