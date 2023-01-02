b5ef5642edcfb22ccd8cf23f818807408bf87c7d
ssppoollsem <ssppoollsem@gmail.com>
2023-01-02 21:37:44 +0900

    리액트 - redux (reducer)

---
+++ react_study/fastcampus/part11-Redux/01-01.redux-start/src/redux/reducers.js
@@ -0,0 +1,20 @@
+import { ADD_TODO } from './actions';
+
+// state
+// ['코딩', '점심 먹기'];
+const initialState = [];
+
+function todoApp(previousState = initialState, action) {
+    // 초기값을 설정해 주는 부분
+    // if (previousState === undefined) {
+    //     return [];
+    // }
+
+    if (action.type === ADD_TODO) {
+        return [...previousState, action.todo];
+    }
+
+    // previousState.push('');
+
+    return previousState;
+}
