7d06d2c517b03b034d41ed76250ffc442c3156cf
ssppoollsem <ssppoollsem@gmail.com>
2023-04-29 18:05:59 +0900

    my-books : signin 프론트 개발

--- react_study/fastcampus/part12.my-books/src/components/Signin.tsx
+++ react_study/fastcampus/part12.my-books/src/components/Signin.tsx
@@ -1,7 +1,16 @@
-import { Row, Col, Input, Button } from 'antd';
+import { Row, Col, Input, Button, InputRef } from 'antd';
 import styles from './Signin.module.css';
+import { useRef } from 'react';
+import { LoginReqType } from '../types';
 
-export default function Signin() {
+interface SiginProps {
+    login: (reqData: LoginReqType) => void;
+}
+
+const Signin: React.FC<SiginProps> = ({ login }) => {
+    const emailRef = useRef<InputRef>(null);
+    const passwordRef = useRef<InputRef>(null);
+
     return (
         <Row align="middle" className={styles.signin_row}>
             <Col span={24}>
@@ -17,16 +26,16 @@ export default function Signin() {
                             Email <span className={styles.required}>*</span>
                         </div>
                         <div className={styles.input_area}>
-                            <Input placeholder="Email" autoComplete="email" name="email" className={styles.input} />
+                            <Input placeholder="Email" autoComplete="email" name="email" className={styles.input} ref={emailRef} />
                         </div>
                         <div className={styles.password_title}>
                             Password <span className={styles.required}>*</span>
                         </div>
                         <div className={styles.input_area}>
-                            <Input type="password" autoComplete="current-password" name="email" className={styles.input} />
+                            <Input type="password" autoComplete="current-password" className={styles.input} ref={passwordRef} />
                         </div>
                         <div className={styles.button_area}>
-                            <Button size="large" className={styles.button}>
+                            <Button size="large" className={styles.button} onClick={click}>
                                 Signin
                             </Button>
                         </div>
@@ -35,4 +44,13 @@ export default function Signin() {
             </Col>
         </Row>
     );
-}
+
+    function click() {
+        const email = emailRef.current!.input!.value;
+        const password = passwordRef.current!.input!.value;
+
+        login({ email, password });
+    }
+};
+
+export default Signin;
--- react_study/fastcampus/part12.my-books/src/containers/SigninContainer.tsx
+++ react_study/fastcampus/part12.my-books/src/containers/SigninContainer.tsx
@@ -1,5 +1,16 @@
+import { useCallback } from 'react';
 import Signin from '../components/Signin';
+import { useDispatch } from 'react-redux';
+import { login as loginSagaStart } from '../redux/modules/auth';
 
 export default function SigninContainer() {
-    return <Signin />;
+    const dispatch = useDispatch();
+
+    const login = useCallback(
+        (reqData: any) => {
+            dispatch(loginSagaStart(reqData));
+        },
+        [dispatch]
+    );
+    return <Signin login={login} />;
 }
