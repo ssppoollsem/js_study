ff424c09d292693fcc21ab58c66b0a25cad828e6
ssppoollsem <ssppoollsem@gmail.com>
2023-04-29 16:19:54 +0900

    my-books : signin 스타일 작업

---
+++ react_study/fastcampus/part12.my-books/src/components/Signin.tsx
@@ -0,0 +1,38 @@
+import { Row, Col, Input, Button } from 'antd';
+import styles from './Signin.module.css';
+
+export default function Signin() {
+    return (
+        <Row align="middle" className={styles.signin_row}>
+            <Col span={24}>
+                <Row className={styles.signin_contents}>
+                    <Col span={12}>
+                        <img src="/bg_signin.png" alt="Signin" className={styles.signin_bg} />
+                    </Col>
+                    <Col span={12}>
+                        <div className={styles.signin_title}>My Books</div>
+                        <div className={styles.signin_subtitle}>Please Note Your Option</div>
+                        <div className={styles.sigin_underline} />
+                        <div className={styles.email_title}>
+                            Email <span className={styles.required}>*</span>
+                        </div>
+                        <div className={styles.input_area}>
+                            <Input placeholder="Email" autoComplete="email" name="email" className={styles.input} />
+                        </div>
+                        <div className={styles.password_title}>
+                            Password <span className={styles.required}>*</span>
+                        </div>
+                        <div className={styles.input_area}>
+                            <Input type="password" autoComplete="current-password" name="email" className={styles.input} />
+                        </div>
+                        <div className={styles.button_area}>
+                            <Button size="large" className={styles.button}>
+                                Signin
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    );
+}
